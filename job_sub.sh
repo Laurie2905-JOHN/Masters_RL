19e1ed585c36e4fb610c469b3e6ae6b74991a138
Laurie Johnston <laurie.johnston@me.com>
2024-06-12 11:57:08 +0100

    best model trained but stoppig at 100k

--- job_sub.sh
+++ job_sub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=10:mem=16gb:ngpus=0
-#PBS -l walltime=4:00:00
-#PBS -N RL_Algo_Test
-#PBS -o /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/algo_test_A2C_1mill.log
-#PBS -e /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/algo_test_A2C_1mill_error.log
+#PBS -l select=1:ncpus=10:mem=64gb:ngpus=0
+#PBS -l walltime=15:00:00
+#PBS -N RL_Algo_Test_GPU
+#PBS -o /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/test2.log
+#PBS -e /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/test2_error.log
 
 cd $PBS_O_WORKDIR
 
@@ -12,7 +12,11 @@ module load anaconda3/personal
 
 source activate MasterEnv
 
-# Run the Python script with A2C algorithm
-python "/rds/general/user/lej23/home/fyp/Masters_RL/scripts/training/train.py" --algo A2C --env_name SchoolMealSelection-v0 --num_envs 4 --total_timesteps 1000000 --save_freq 10000 --eval_freq 100
-# Run the Python script with PPO algorithm
-# python "/rds/general/user/lej23/home/fyp/Masters_RL/scripts/training/train.py" --algo PPO --env_name SchoolMealSelection-v0 --num_envs 4 --total_timesteps 100000 --save_freq 10000 --eval_freq 100 
+python "scripts/training/train.py" \
+  --algo="A2C" \
+  --env_name="SchoolMealSelection-v1" \
+  --num_envs=3 \
+  --total_timesteps=3000000 \
+  --save_freq=1000 \
+  --eval_freq=1000 \
+  --device="cuda" \
