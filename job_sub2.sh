aea0f9c6befdb76f01bf05ef275a210b6b3dba79
Laurie Johnston <laurie.johnston@me.com>
2024-06-13 11:31:31 +0100

    merged

--- job_sub2.sh
+++ job_sub2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=16:mem=64gb:ngpus=4
+#PBS -l select=1:ncpus=10:mem=16gb:ngpus=1
 #PBS -l walltime=8:00:00
-#PBS -N PPO_Optuna_Optimization
-#PBS -o /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/ppo_optimization.log
-#PBS -e /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/ppo_optimization_error.log
+#PBS -N RL_Algo_Test
+#PBS -o /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/algo_test_gpu_env3.log
+#PBS -e /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/algo_test_error_gpu_env3.log
 
 cd $PBS_O_WORKDIR
 
@@ -12,5 +12,7 @@ module load anaconda3/personal
 
 source activate MasterEnv
 
-# Run the Python script with PPO algorithm for Optuna optimization
-python "/rds/general/user/lej23/home/fyp/Masters_RL/scripts/training/train.py" --algo PPO --n_trials 1000 --timeout 28800 --n_jobs 16 --n_gpus 4
+# Run the Python script with A2C algorithm
+python "/rds/general/user/lej23/home/fyp/Masters_RL/scripts/training/train.py" --algo A2C --env_name SchoolMealSelection-v0 --num_envs 4 --total_timesteps 100000 --save_freq 10000 --eval_freq 100
+# Run the Python script with PPO algorithm
+python "/rds/general/user/lej23/home/fyp/Masters_RL/scripts/training/train.py" --algo PPO --env_name SchoolMealSelection-v0 --num_envs 4 --total_timesteps 100000 --save_freq 10000 --eval_freq 100 
