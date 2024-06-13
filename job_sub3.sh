8479bee4720687cb1307e925fd5aa88c5a36c0ce
Laurie Johnston <laurie.johnston@me.com>
2024-06-13 11:52:36 +0100

    updated gitignore to include evaluation models and ignore tensorboard

--- job_sub3.sh
+++ job_sub3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=16:mem=64gb:ngpus=4
+#PBS -l select=1:ncpus=16:mem=64gb:ngpus=0
 #PBS -l walltime=8:00:00
 #PBS -N A2C_Optuna_Optimization
 #PBS -o /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/a2c_optimization.log
@@ -13,4 +13,4 @@ module load anaconda3/personal
 source activate MasterEnv
 
 # Run the Python script with A2C algorithm for Optuna optimization
-python "/rds/general/user/lej23/home/fyp/Masters_RL/scripts/training/train.py" --algo A2C --n_trials 100 --timeout 28800 --n_jobs 16 --n_gpus 4
+python "/rds/general/user/lej23/home/fyp/Masters_RL/scripts/training/hyper_tune.py" --algo A2C --n_trials 100 --timeout 28800 --n_jobs 16 --n_gpus 0
