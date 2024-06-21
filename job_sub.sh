2003f651915814266f21e62013bbc8c8169535b1
Laurie Johnston <laurie.johnston@me.com>
2024-06-21 18:52:30 +0100

    updated git ignore

--- job_sub.sh
+++ job_sub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=32:mem=120gb:ngpus=0
+#PBS -l select=1:ncpus=32:mem=64gb:ngpus=0
 #PBS -l walltime=24:00:00
-#PBS -N parralel_optuna6
-#PBS -o /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/parralel_optuna6.log
-#PBS -e /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/parralel_optuna_error6.log
+#PBS -N parralel_optuna13
+#PBS -o /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/parralel_optuna13.log
+#PBS -e /rds/general/user/lej23/home/fyp/Masters_RL/saved_models/hpc_output/parralel_optuna_error13.log
 
 cd $PBS_O_WORKDIR
 
