f3c9ffa7216d43f81e5e25e9d572d7ce48ec6d69
Laurie2905-JOHN <laurie.johnston@me.com>
2024-06-24 17:05:13 +0100

    updates to run PPO optimisation

--- scripts/training/hyperparam_search/optuna_search.py
+++ scripts/training/hyperparam_search/optuna_search.py
@@ -46,7 +46,7 @@ def objective(trial: optuna.Trial, ingredient_df, study_path, num_timesteps, alg
     # Example usage within your make_env function
     def make_env():
         env = gym.make("SchoolMealSelection-v0", **env_kwargs)
-        env = TimeLimit(env, max_episode_steps=1000)
+        env = TimeLimit(env, max_episode_steps=100)
         
         # Normalize observations and rewards
         env = NormalizeDictObservation(env)
@@ -68,7 +68,7 @@ def objective(trial: optuna.Trial, ingredient_df, study_path, num_timesteps, alg
     else:
         raise ValueError("Invalid algorithm")
     
-    stop_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=30, min_evals=50, verbose=1)
+    stop_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=20, min_evals=50, verbose=1)
     
     eval_callback = TrialEvalCallback(
         env, trial, best_model_save_path=path, log_path=path,
@@ -169,16 +169,16 @@ def main(algo, study_name, storage, n_trials, timeout, n_jobs, num_timesteps):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--algo', type=str, default="PPO", help="Algorithm to optimize: PPO or A2C")
+    parser.add_argument('--algo', type=str, default="A2C", help="Algorithm to optimize: PPO or A2C")
     parser.add_argument('--study_name', type=str, default=None, help="Name of the Optuna study")
     parser.add_argument('--storage', type=str, default=None, help="Database URL for Optuna storage")
-    parser.add_argument('--n_trials', type=int, default=1, help="Number of trials for optimization")
+    parser.add_argument('--n_trials', type=int, default=10, help="Number of trials for optimization")
     parser.add_argument('--timeout', type=int, default=86400, help="Timeout for optimization in seconds")
-    parser.add_argument('--n_jobs', type=int, default=1, help="Number of jobs to assign")
-    parser.add_argument('--num_timesteps', type=int, default=10000000, help="Number of timesteps for model training")
+    parser.add_argument('--n_jobs', type=int, default=2, help="Number of jobs to assign")
+    parser.add_argument('--num_timesteps', type=int, default=1000000, help="Number of timesteps for model training")
     args = parser.parse_args()
     
     if args.study_name is None:
-        args.study_name = f"{args.algo}_study1"
+        args.study_name = f"{args.algo}_study_laptop_1mil"
         
     main(args.algo, args.study_name, args.storage, args.n_trials, args.timeout, args.n_jobs, args.num_timesteps)
